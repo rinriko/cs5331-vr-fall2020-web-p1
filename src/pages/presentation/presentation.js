93ac9c4cee9e74087e836465436b737c81baead8
Zipexpo <nvtngan@hcmus.edu.vn>
2020-10-07 15:17:11 -0500

    Update presentation.js

--- src/pages/presentation/presentation.js
+++ src/pages/presentation/presentation.js
@@ -99,7 +99,7 @@ class PresentationPage extends React.Component {
 
             let data = keys.map((d,i) => {
                 return {
-                    y: keys.length-i-1,
+                    y: i,
                     x: score[d]
                 }
             })
